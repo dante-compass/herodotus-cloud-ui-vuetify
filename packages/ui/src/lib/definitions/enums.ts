30f9f4ba745fec6de178ef63cb27f214ac85e448
码匠君 <pointer_v@qq.com>
2025-07-23 15:16:30 +0800

    feat: 提交变更

--- packages/ui/src/lib/definitions/enums.ts
+++ packages/ui/src/lib/definitions/enums.ts
@@ -10,16 +10,3 @@ export enum OperationEnum {
   SETUP = 'setup',
   INVOKE = 'invoke',
 }
-
-export enum LayoutModeEnum {
-  DEFAULT = 'defaults',
-  CLASSIC = 'classic',
-  TRANSVERSE = 'transverse',
-  COLUMNS = 'transverse',
-}
-
-export enum ThemeModeEnum {
-  DARK = 'dark',
-  LIGHT = 'light',
-  SYSTEM = 'system',
-}
