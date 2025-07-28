862f10afef4ded908b1ce0cc823360b05846b4ba
码匠君 <pointer_v@qq.com>
2025-07-28 12:24:09 +0800

    feat: 修改代码错误

--- packages/ui/src/lib/declarations/route.ts
+++ packages/ui/src/lib/declarations/route.ts
@@ -1,30 +1,9 @@
-import type { Tree } from './base';
-
 export interface MenuItem {
   title: string | unknown;
   prependIcon: string | unknown;
   to?: string;
   value?: string;
   $children?: MenuItem[];
-}
-
-export interface RemoteRouteMeta {
-  title: string;
-  icon: string;
-  sort: number;
-  isHaveChild?: boolean;
-  isNotKeepAlive?: boolean;
-  isHideAllChild?: boolean;
-  isDetailContent?: boolean;
-  isIgnoreAuth?: boolean;
-}
-export interface RemoteRoute extends Tree {
-  componentName: string;
-  componentPath: string;
-  redirect: string;
-  meta: RemoteRouteMeta;
-  roles: Array<string>;
-  children?: Array<RemoteRoute>;
 }
 
 export interface WidgetItem {
