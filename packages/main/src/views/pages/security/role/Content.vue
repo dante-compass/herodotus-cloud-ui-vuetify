5393690107b51d54e70d6be080efe79bd2da05a0
码匠君 <pointer_v@qq.com>
2025-11-13 15:31:44 +0800

    feat: 迁移页面

--- packages/main/src/views/pages/security/role/Content.vue
+++ packages/main/src/views/pages/security/role/Content.vue
@@ -3,5 +3,18 @@
 </template>
 
 <script setup lang="ts">
-defineOptions({ name: 'OAuth2ApplicationContent' });
+import type { SysAttributeEntity } from '@herodotus/api';
+
+import { useTableItem } from '@/composables/hooks';
+import { API } from '@/configurations';
+
+defineOptions({ name: 'SysAttributeContent' });
+
+const { editedItem, operation, title, saveOrUpdate } = useTableItem<SysAttributeEntity>(
+  API.core.sysAttribute(),
+);
+
+const onSave = () => {
+  saveOrUpdate();
+};
 </script>
