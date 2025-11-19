a77450b3301211cd661a1aa667b0a2e742c8f854
码匠君 <pointer_v@qq.com>
2025-11-20 01:21:04 +0800

    feat: 提交树形组件

---
+++ packages/main/src/views/pages/security/element/HTreeField.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-text-field label="ceshi" v-bind="$attrs">
+    <v-menu activator="parent">
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-text-field>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'HTreeField' });
+
+const items = [
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me 2' },
+];
+</script>
