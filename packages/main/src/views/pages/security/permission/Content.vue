6898288aadbebfbab0328dca1092f6e71ebd1e11
码匠君 <pointer_v@qq.com>
2025-11-19 14:54:19 +0800

    feat: 权限管理完成

--- packages/main/src/views/pages/security/permission/Content.vue
+++ packages/main/src/views/pages/security/permission/Content.vue
@@ -1,20 +1,47 @@
 <template>
-  <div>ApplicationContent</div>
+  <h-center-layout-container
+    :entity="editedItem"
+    :title="title"
+    :operation="operation"
+    @save="onSave()"
+  >
+    <v-form ref="permissionForm" validate-on="blur lazy">
+      <v-text-field
+        v-model.lazy="editedItem.permissionName"
+        label="权限名称 * "
+        placeholder="请输入权限名称"
+        :rules="[
+          (v: string) => !!v || '权限名称不能为空',
+          (v: string) => (v && v.length >= 5) || '权限名称至少5个字符',
+        ]"
+      ></v-text-field>
+      <v-text-field
+        v-model.lazy="editedItem.permissionCode"
+        label="权限代码"
+        placeholder="请输权限代码"
+      ></v-text-field>
+    </v-form>
+  </h-center-layout-container>
 </template>
 
 <script setup lang="ts">
-import type { SysAttributeEntity } from '@herodotus/api';
+import type { SysPermissionEntity } from '@herodotus/api';
 
 import { useTableItem } from '@/composables/hooks';
 import { API } from '@/configurations';
 
 defineOptions({ name: 'SysPermissionContent' });
 
-const { editedItem, operation, title, saveOrUpdate } = useTableItem<SysAttributeEntity>(
-  API.core.sysAttribute(),
+const permissionForm = ref();
+
+const { editedItem, operation, title, saveOrUpdate } = useTableItem<SysPermissionEntity>(
+  API.core.sysPermission(),
 );
 
-const onSave = () => {
-  saveOrUpdate();
+const onSave = async () => {
+  const { valid } = await permissionForm.value.validate();
+  if (valid) {
+    saveOrUpdate();
+  }
 };
 </script>
