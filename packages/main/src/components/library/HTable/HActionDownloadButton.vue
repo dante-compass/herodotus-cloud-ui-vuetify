b135b208348068af9781f94fe71fb6c46c019eac
码匠君 <pointer_v@qq.com>
2026-03-05 23:03:14 +0800

    feat: 调整页面

--- packages/main/src/components/library/HTable/HActionDownloadButton.vue
+++ packages/main/src/components/library/HTable/HActionDownloadButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <h-action-button color="secondary" icon="mdi-file-download" tooltip="下载文件" v-bind="$attrs"></h-action-button>
+  <h-action-button color="indigo" icon="mdi-cloud-download" tooltip="下载文件" v-bind="$attrs"></h-action-button>
 </template>
 
 <script setup lang="ts">
